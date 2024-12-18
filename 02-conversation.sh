b255448d01b8192d822b887e7c0c85d09f2db7d0
Bulliraju <bulliraju118@gmail.com>
2024-12-18 16:17:06 +0530

    I added conversation

---
+++ 02-conversation.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+person1=Sriram
+person2=Prasad
+
+echo "Sriram :: hi prasad, how are you?"
+echo "Prasad :: Hello Sriram. I am fine. How are you?"
+echo "Sriram :: I am doing good prasad. what's goging on?"
+echo "Prasad :: I started learning shell script Sriram"
