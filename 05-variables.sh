e758561b7d9d9c8e2d6f657060afaf1901e9771d
Bulliraju <bulliraju118@gmail.com>
2024-12-18 18:50:31 +0530

    i added variable file and array file

---
+++ 05-variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Please enter username::"
+read -s USERNAME #here USERNAME IS VARIABLE
+echo "Please enter password::"
+read -s PASSWORD
+echo "Username is : $USERNAME, Password is: $PASSWORD"
---
+++ 06-array.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+MOVIE=("RRR" "DJTILLU" "MURARI")
+#sizze of above array is 3.
+#index are 0,1,2
+
+echo "first movie is : ${MOVIE[0]}"
+echo "Second movie is : ${MOVIE[1]}"
+echo "Third movie is : ${MOVIE[2]}"
