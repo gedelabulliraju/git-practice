332acb55472e4d95c91b47f75482a5be27a8ecdc
Bulliraju <bulliraju118@gmail.com>
2024-12-18 17:45:21 +0530

    I added conversation with variables

---
+++ 03-conversation.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+$person1=sriram
+$person2=Prasad
+
+echo "$person1 :: hi prasad, how are you?"
+echo "$person2 :: Hello $person1. I am fine. How are you?"
+echo "$person1 :: I am doing good prasad. what's goging on?"
+echo "$person2 :: I started learning shell script $person1"
