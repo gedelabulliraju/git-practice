5ef77258fa987d8966ce66b9d4e23af984678282
Bulliraju <bulliraju118@gmail.com>
2024-12-18 17:52:22 +0530

    i modify some data

--- 03-conversation.sh
+++ 03-conversation.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-$person1=sriram
-$person2=Prasad
+sriram=$[person1]
+Prasad=$[person2]
 
-echo "$person1 :: hi prasad, how are you?"
-echo "$person2 :: Hello $person1. I am fine. How are you?"
-echo "$person1 :: I am doing good prasad. what's goging on?"
-echo "$person2 :: I started learning shell script $person1"
+
+echo "$[person1] :: hi prasad, how are you?"
+echo "$[person2] :: Hello $[person1]. I am fine. How are you?"
+echo "$[person1] :: I am doing good prasad. what's goging on?"
+echo "$[person2] :: I started learning shell script $[person1]"
