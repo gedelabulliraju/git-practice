8f73b0c0ae44cd298cb437bef6de3a12c966ef27
Bulliraju <bulliraju118@gmail.com>
2024-12-20 19:45:06 +0530

    i modified some error

--- 09-spl_variables.sh
+++ 09-spl_variables.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo "All variables passed to the script: $@"
-echo"Number of variables passed: $#"
-echo"Script name: $0"
-echo"Current working directory: $pwd"
-echo"home directory if current user :$home"
-echo"PID of the script executing now: $$"
+echo "Number of variables passed: $#"
+echo "Script name: $0"
+echo "Current working directory: $pwd"
+echo "home directory if current user :$home"
+echo "PID of the script executing now: $$"
 sleep 100 &
 echo "PID of last background commandL:$!" 
