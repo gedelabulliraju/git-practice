ef30150218d77bb4cf8500bfc083e3739215b6b1
Bulliraju <bulliraju118@gmail.com>
2024-12-21 11:20:24 +0530

    I added some data

--- 09-spl_variables.sh
+++ 09-spl_variables.sh
@@ -2,9 +2,9 @@
 
 echo "All variables passed to the script: $@"
 echo "Number of variables passed: $#"
-echo "Script name: $0"
+echo "Script name: $0 "
 echo "Current working directory: $pwd"
-echo "home directory if current user :$home"
+echo "home directory if current user : $home"
 echo "PID of the script executing now: $$"
 sleep 100 &
-echo "PID of last background commandL:$!" 
+echo "PID of last background commandL:$!"
