d3519886d738ddcc6535b55b16a0ad7bae3875e4
Bulliraju <bulliraju118@gmail.com>
2024-12-21 11:35:23 +0530

    I modified variable data

--- 09-spl_variables.sh
+++ 09-spl_variables.sh
@@ -3,8 +3,8 @@
 echo "All variables passed to the script: $@"
 echo "Number of variables passed: $#"
 echo "Script name: $0 "
-echo "Current working directory: $pwd"
-echo "home directory if current user : $home"
+echo "Current working directory: $PWD"
+echo "home directory if current user : $HOME"
 echo "PID of the script executing now: $$"
 sleep 100 &
 echo "PID of last background commandL:$!"
