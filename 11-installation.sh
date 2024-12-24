05d443e913c4e89e0e70fc0ed9de7171b0b4801d
Bulliraju <bulliraju118@gmail.com>
2024-12-24 21:43:06 +0530

    I added some data

--- 11-installation.sh
+++ 11-installation.sh
@@ -3,6 +3,7 @@
 USERID=$(id -u)
 
 if [ $USERID -ne 0 ]
-then echo"Please run this script with root preveleses"
-exit 1
+then 
+    echo "Please run this script with root preveleses"
+    exit 1
 fi
