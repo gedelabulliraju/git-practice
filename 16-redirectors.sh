5872aacd2a22626c4ba9c193ada11fc8073beabf
Bulliraju <bulliraju118@gmail.com>
2024-12-29 20:16:11 +0530

    I added logs to shell script

--- 16-redirectors.sh
+++ 16-redirectors.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 LOGS_FOLDER="/var/log/shell-script"
-SCRIPT_NAME=$(echo "$0" | cut -d "." -f1)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
-LOG_FILE="$LOGS_FOLDER/SCRIPT_NAME-$TIMESTAMP.log"
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
 mkdir -p $LOGS_FOLDER
 
 
