7f8ca303bb0b58c0c17e78085b2adf17600aab0a
Bulliraju <bulliraju118@gmail.com>
2024-12-29 18:49:09 +0530

    I added logs to shell script

--- 16-redirectors.sh
+++ 16-redirectors.sh
@@ -22,10 +22,10 @@ VALIDATE()
 {
     if [ $1 -ne 0 ]
     then
-    echo -e "$2 is $R failure $N" | tee -a $LOG_FILE
+    echo -e "$2 is $R failure $N" 
     exit 1
     else
-    echo -e "$2 is $G success $N" | tee -a $LOG_FILE
+    echo -e "$2 is $G success $N" 
     fi
 }
 
