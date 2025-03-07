d2f4647316d081b7e830bc7c0f3e8d4f63cdb572
Bulliraju <bulliraju118@gmail.com>
2025-03-07 10:48:06 +0530

    i added files

--- 16-redirectors.sh
+++ 16-redirectors.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 LOGS_FOLDER="/var/log/shell-script"
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+TIMESTAMP=$( date +%Y-%m-%d-%H-%M-%S )
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
 mkdir -p $LOGS_FOLDER
 
--- 16-redirectors1.sh
+++ 16-redirectors1.sh
@@ -3,8 +3,8 @@
 # Set up logging
 LOGS_FOLDER="/var/log/shell-script"
 SCRIPT_NAME=$(echo "$0" | cut -d "." -f1)
-TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
+TIME_STAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIME_STAMP.log"
 
 # Create logs folder if it doesn't exist
 mkdir -p "$LOGS_FOLDER"
